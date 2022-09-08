e207bbb5d4e9f6812753a8fb6d4b16a9e19b9745
abenteuerzeit <amroz.muzyka@gmail.com>
2022-09-08 10:47:41 +0200

    Test BinaryTree

---
+++ BinaryTree Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using BinaryTree;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Tree<int> intTree = new Tree<int>(10);
+        intTree.Insert(5);
+        intTree.Insert(11);
+        intTree.Insert(5);
+        intTree.Insert(-12);
+        intTree.Insert(15);
+        intTree.Insert(0);
+        intTree.Insert(14);
+        intTree.Insert(-8);
+        intTree.Insert(10);
+        intTree.Insert(8);
+        intTree.Insert(8);
+        string sortedData = intTree.WalkTree();
+        Console.WriteLine($"Sorted data is: {sortedData}");
+
+        Tree<string> stringTree = new Tree<string>("Hello");
+        stringTree.Insert("World");
+        stringTree.Insert("How");
+        stringTree.Insert("Are");
+        stringTree.Insert("You");
+        stringTree.Insert("Today");
+        stringTree.Insert("I");
+        stringTree.Insert("Hope");
+        stringTree.Insert("You");
+        stringTree.Insert("Are");
+        stringTree.Insert("Feeling");
+        stringTree.Insert("Well");
+        stringTree.Insert("!");
+        sortedData = stringTree.WalkTree();
+        Console.WriteLine($"Sorted data is: {sortedData}");
+    }
+}
