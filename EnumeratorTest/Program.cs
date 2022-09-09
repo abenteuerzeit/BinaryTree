4f2e86587d6b0af439fefae9b9cec5f5bc621e4c
abenteuerzeit <amroz.muzyka@gmail.com>
2022-09-09 11:00:36 +0200

    Test Enumerator

---
+++ EnumeratorTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using BinaryTree;
+
+namespace EnumeratorTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> intTree = new(10);
+            intTree.Insert(5);
+            intTree.Insert(11);
+            intTree.Insert(5);
+            intTree.Insert(-12);
+            intTree.Insert(15);
+            intTree.Insert(0);
+            intTree.Insert(14);
+            intTree.Insert(-8);
+            intTree.Insert(10);
+            intTree.Insert(8);
+            intTree.Insert(8);
+
+            foreach (int integer in intTree)
+            {
+                Console.WriteLine(integer);
+            }
+
+        }
+    }
+}
